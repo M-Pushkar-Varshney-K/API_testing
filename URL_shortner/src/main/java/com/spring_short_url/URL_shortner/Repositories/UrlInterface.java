6ccf17976b4cf30636823e85835d19d48eec9370
Vikas <xanderbilla@gmail.com>
2025-05-31 20:59:05 +0530

    correcting type error and good practice

--- URL_shortner/src/main/java/com/spring_short_url/URL_shortner/Repositories/UrlInterface.java
+++ URL_shortner/src/main/java/com/spring_short_url/URL_shortner/Repositories/UrlInterface.java
@@ -1,12 +1,9 @@
 package com.spring_short_url.URL_shortner.Repositories;
 
-import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.mongodb.repository.Query;
 
 import com.spring_short_url.URL_shortner.Models.Urls;
 
-public interface UrlInterface extends MongoRepository<Urls, ObjectId> {
-    @Query("{ 'id' : ?0 }")
-    Urls findById(String id);
+public interface UrlInterface extends MongoRepository<Urls, String> {
+    // Custom query methods can be added here if needed
 }
--- URL_shortner/src/main/java/com/spring_short_url/URL_shortner/Services/MongoService.java
+++ URL_shortner/src/main/java/com/spring_short_url/URL_shortner/Services/MongoService.java
@@ -1,14 +1,14 @@
 package com.spring_short_url.URL_shortner.Services;
 
-import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.boot.autoconfigure.security.SecurityProperties.User;
+import java.time.LocalDateTime;
+import java.util.Optional;
+
 import com.spring_short_url.URL_shortner.Models.Users;
 import com.spring_short_url.URL_shortner.Models.Urls;
 import com.spring_short_url.URL_shortner.Repositories.UsersInterface;
 import com.spring_short_url.URL_shortner.Repositories.UrlInterface;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
-// import org.bson.types.ObjectId;
 
 @Service
 @Component
@@ -16,26 +16,34 @@ public class MongoService {
     private final UrlInterface urlRepo;
     private final UsersInterface userRepo;
 
-    @Autowired
     public MongoService(UrlInterface urlRepo, UsersInterface userRepo) {
         this.urlRepo = urlRepo;
         this.userRepo = userRepo;
     }
 
-
     public void saveUser(Users user) {
         userRepo.save(user);
+    }
+
+    public Urls createShortUrl(Urls url) {
+        url.setCreatedAt(LocalDateTime.now());
+        String code = Integer.toHexString(url.getLongURL().hashCode());
+        String shortUrl = "http://localhost:8080/" + code;
+        url.setShortURL(shortUrl);
+        url.setId(code);
+        url.setExpired(url.getCreatedAt().plusDays(1));
+        saveUrl(url);
+        return url;
     }
 
     public void saveUrl(Urls url) {
         urlRepo.save(url);
     }
 
-    public String getFullUrl(String hexcode){
-        // ObjectId id = new ObjectId(hexcode);
-        Urls url = urlRepo.findById(hexcode);
-        if(url != null){
-            return url.getLongURL();
+    public String getFullUrl(String hexcode) {
+        Optional<Urls> url = urlRepo.findById(hexcode);
+        if (url.isPresent()) {
+            return url.get().getLongURL();
         }
         return "";
     }
